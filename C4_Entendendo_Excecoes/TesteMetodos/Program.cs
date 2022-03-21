a1422f0c1d6e693f3097a8de97f25f646fd18db5
Juddy Lopes <juddykelly.jk@gmail.com>
2022-03-21 19:46:18 -0300

    Parte 4 do curso de C# orientação a objetos iniciada

---
+++ C4_Entendendo_Excecoes/TesteMetodos/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Metodo();
+        }
+        catch (DivideByZeroException excecao)
+        {
+            Console.WriteLine("Exceção tratada.");
+            Console.WriteLine(excecao.Message);
+            Console.WriteLine(excecao.StackTrace);
+        }
+        catch (NullReferenceException excecao)
+        {
+            Console.WriteLine("Exceção tratada.");
+            Console.WriteLine(excecao.Message);
+            Console.WriteLine(excecao.StackTrace);
+        }
+    static void Metodo()
+    {
+        try
+        {
+            TestaDivisao(0);
+        }
+        catch (NullReferenceException excecao)
+        {
+            Console.WriteLine(excecao.Message);
+            Console.WriteLine(excecao.StackTrace);
+        }
+        
+    }
+
+    static void TestaDivisao(int divisor)
+    {
+        Dividir(10, divisor);
+    }
+    public static int Dividir(int numero, int divisor)
+    {
+        ContaCorrente conta = null;
+        Console.WriteLine(conta.Saldo);
+        return numero / divisor;
+    }
+
+
+
+
+}
